13169c214cfe67b6557acf4b257e0dee2bacf2f3
Angelica Bufano <abufano@tql.com>
2024-06-11 14:51:59 -0400

    Add project files.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFirstConsoleApp;
+
+internal class Program {
+    public static string Name { get; set; } = "Angelica";
+    static void Main(string[] args) {
+        Console.WriteLine($"Hello, {Name} World!");
+
+    }
+}
+
